ea204f882269f9f18087ca812945eb16bc4445c7
rvaramees <146175672+rvaramees@users.noreply.github.com>
2024-01-31 17:01:37 +0530

    update:menu driven

--- lab_programs/queue_array.c
+++ lab_programs/queue_array.c
@@ -1,12 +1,13 @@
-#define MAX_SIZE 100  
 #include<stdio.h>
 #include<stdlib.h>  
+#define MAX_SIZE 5
 int queue[MAX_SIZE];  
 int front = -1;  
 int rear = -1;  
-int i;
-  
-void enqueue(int element) {  
+void enqueue() {  
+    int element;
+    printf("Enter the element : ");
+    scanf("%d", &element);
     if (rear == MAX_SIZE - 1) {  
         printf("Queue is full");  
         return;  
@@ -25,10 +26,11 @@ int dequeue() {
     }  
     int element = queue[front];  
     front++;  
-    return element;  
+    printf("\nDequeued element %d ", element);
 }  
 
 int display() {
+    int i;
     if(front == -1) {
         printf("empty!");
         return 0;
@@ -40,15 +42,25 @@ int display() {
 }  
 
 int main() {  
-    enqueue(10);  
-    enqueue(20);  
-    enqueue(30);
-    enqueue(40);  
-    enqueue(50);
-    enqueue(60);
-    printf("%d ", dequeue());  
-    printf("%d ", dequeue());  
-    printf("%d ", dequeue());  
-    display();
-    return 0;  
+    int choice;
+    while (1) {
+        printf("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice : ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                enqueue();
+                break;
+            case 2 :
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+                break;
+            default:
+                printf("Invalid Syntax !");
+        }
+    }
 }  
